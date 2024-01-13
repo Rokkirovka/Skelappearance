1df331486d6f0e3cb31d5be24c169cf4b555e541
Rokkirovka <adelgooglagel@gmail.com>
2024-01-13 15:32:53 +0500

    turns_and_base_AI

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 font = pygame.font.SysFont('Arial', 40)
 colors = {True: 'blue', False: 'red'}
+moving = False
+your_turn = False
 
 
 def load_image(name, colorkey=None):
@@ -37,7 +39,7 @@ class Scene:
         self.enemies = [None, None, None]
         self.enemies_bars = [None, None, None]
         self.selected = None
-        self.turning = 0
+        self.pers_turning = 0
 
     def add_character(self, char):
         bar = HPBar(char.hp)
@@ -61,7 +63,7 @@ class Scene:
 
 
 class Person(pygame.sprite.Sprite):
-    def __init__(self, side, position, hero, hp=1000, damage=100, armor=5, speed=10):
+    def __init__(self, side, position, hero, skills, hp=1000, damage=100, armor=5, speed=10):
         super().__init__(all_sprites)
         self.radius = 40
         self.hp = hp
@@ -71,16 +73,25 @@ class Person(pygame.sprite.Sprite):
         self.position = position
         self.side = side
         self.hero = hero
+        self.skills = skills
         self.image = pygame.Surface((2 * self.radius, 2 * self.radius), pygame.SRCALPHA, 32)
         self.rect = None
 
-    def attack(self, skill, target, bar):
+    def attack(self, skill, target):
         target.hp -= self.damage * skill.coefficient
+        if target.side:
+            bar = scene.heroes_bars[target.position]
+        else:
+            bar = scene.enemies_bars[target.position]
         bar.hp -= self.damage * skill.coefficient
 
     def update(self, *args):
         if self.hp <= 0:
             self.kill()
+            if self in scene.heroes:
+                scene.heroes[scene.heroes.index(self)] = None
+            else:
+                scene.enemies[scene.enemies.index(self)] = None
 
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos) and not self.side:
             scene.selected = scene.enemies.index(self)
@@ -97,6 +108,10 @@ class HPBar(pygame.sprite.Sprite):
     def update(self, *args):
         if self.hp <= 0:
             self.kill()
+            if self in scene.heroes_bars:
+                scene.heroes_bars[scene.heroes_bars.index(self)] = None
+            else:
+                scene.enemies_bars[scene.enemies_bars.index(self)] = None
 
         self.percent = int(self.hp / self.max_hp * 100)
         pygame.draw.rect(self.image, 'red', (self.percent * 2.5, 2, 250 - self.percent * 2.5 - 2, 26))
@@ -114,15 +129,41 @@ class Skill(pygame.sprite.Sprite):
         self.rect = pygame.Rect((575, 740, 50, 50))
 
     def update(self, *args):
-        if scene.selected is not None and args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
-            scene.heroes[1].attack(self, scene.enemies[scene.selected], scene.enemies_bars[scene.selected])
+        global your_turn, moving
+        if your_turn:
+            if scene.selected is not None and args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+                scene.heroes[1].attack(self, scene.enemies[scene.selected])
+                your_turn = False
+                scene.pers_turning += 1
+                moving = False
 
 
 strike = Skill(1, True, False)
 
 
 def next_turn():
-    pass
+    global moving, your_turn
+    moving = True
+    if scene.pers_turning % 6 < 3:
+        active = scene.heroes[scene.pers_turning % 3]
+    else:
+        active = scene.enemies[scene.pers_turning % 3]
+    if active is not None:
+        if not active.hero:
+            if active.side:
+                targets = [x for x in scene.enemies if x is not None]
+            else:
+                targets = [x for x in scene.heroes if x is not None]
+            target = random.choice(targets)
+            technique = random.choice(active.skills)
+            active.attack(technique, target)
+            scene.pers_turning += 1
+            moving = False
+        else:
+            your_turn = True
+    else:
+        scene.pers_turning += 1
+        moving = False
 
 
 def terminate():
@@ -131,12 +172,13 @@ def terminate():
 
 
 scene = Scene()
-scene.add_character(Person(True, 0, False, 1000))
-scene.add_character(Person(True, 1, True, 1500))
-scene.add_character(Person(True, 2, False, 1000))
-scene.add_character(Person(False, 0, False, 1000))
-scene.add_character(Person(False, 1, False, 1000))
-scene.add_character(Person(False, 2, False, 1000))
+scene.add_character(Person(True, 0, False, [strike]))
+sonny = Person(True, 1, True, [strike], 1500)
+scene.add_character(sonny)
+scene.add_character(Person(True, 2, False, [strike]))
+scene.add_character(Person(False, 0, False, [strike]))
+scene.add_character(Person(False, 1, False, [strike]))
+scene.add_character(Person(False, 2, False, [strike]))
 
 
 while True:
@@ -149,5 +191,7 @@ while True:
     pygame.draw.rect(screen, 'cyan', (0, 0, width, 300))
     pygame.draw.rect(screen, 'gray', (0, 300, width, height))
     all_sprites.draw(screen)
-    next_turn()
+    if not moving:
+        scene.selected = None
+        next_turn()
     clock.tick(FPS)
